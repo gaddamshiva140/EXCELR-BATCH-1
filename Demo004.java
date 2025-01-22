8684c16a9f6871ed586e6e00fdd6f3f8e55cb6e3
GADDAM SHIVA <gaddamshiva140@gmail.com>
2025-01-22 12:28:00 +0530

    Print 1 to 100 Numbers

--- Demo004.java
+++ Demo004.java
@@ -1,4 +1,4 @@
-public class printnumbers {
+public class Demo004 {
         public static void main(String[] args) {
             for (int i = 1; i <= 100; i++) {
                 System.out.println(i);
